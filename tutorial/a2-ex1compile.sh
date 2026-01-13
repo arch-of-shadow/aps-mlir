7802fde4caa19e5146da2c0bc6b3abee805efb59
zouyuyang <zouyuyang@buaa.edu.cn>
2026-01-13 22:31:03 +0800

    wrong filename

--- tutorial/a2-ex1compile.sh
+++ tutorial/a2-ex1compile.sh
@@ -7,4 +7,4 @@ fi
 
 mkdir -p $APS/tutorial/outputs
 
-pixi run compile $APS/tutorial/cadl/v3ddist_vv_cy.cadl $APS/tutorial/csrc/test_v3ddist_vv.c $APS/tutorial/outputs/v3ddist_vv.riscv
+pixi run compile $APS/tutorial/cadl/v3ddist_vv.cadl $APS/tutorial/csrc/test_v3ddist_vv.c $APS/tutorial/outputs/v3ddist_vv.riscv
